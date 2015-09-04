2d2e4def5927a9a385e66114188e81698bfb4fd6
Albert Liu <albertliu@naturalwellbeing.com>
2015-09-04 13:56:56 -0700

    add gem spec

--- lib/spree/active_shipping/ups_override.rb
+++ lib/spree/active_shipping/ups_override.rb
@@ -176,8 +176,8 @@ module Spree
               rate_estimates = xml.root.css('> RatedShipment').map do |rated_shipment|
                 service_code    = rated_shipment.at('Service/Code').text
                 negotiated_rate = rated_shipment.at('NegotiatedRates/NetSummaryCharges/GrandTotal/MonetaryValue').try(:text)
-                total_price     = negotiated_rate.blank? ? rated_shipment.at('TotalCharges/MonetaryValue').try(:text).to_f : negotiated_rate.to_f
-                currency        = negotiated_rate.blank? ? rated_shipment.at('TotalCharges/CurrencyCode').text : rated_shipment.at('NegotiatedRates/NetSummaryCharges/GrandTotal/CurrencyCode').text
+                total_price     = negotiated_rate.blank? ? rated_shipment.at('RatedPackage/TotalCharges/MonetaryValue').try(:text).to_f : negotiated_rate.to_f
+                currency        = negotiated_rate.blank? ? rated_shipment.at('RatedPackage/TotalCharges/CurrencyCode').text : rated_shipment.at('NegotiatedRates/NetSummaryCharges/GrandTotal/CurrencyCode').text
 
                 ::ActiveShipping::RateEstimate.new(origin, destination, ::ActiveShipping::UPS.name,
                     service_name_for(origin, service_code),
--- spree_active_shipping.gemspec
+++ spree_active_shipping.gemspec
@@ -10,14 +10,14 @@ Gem::Specification.new do |s|
   s.homepage    = 'http://github.com/spree/spree_active_shipping'
   s.summary     = 'Spree extension for providing shipping methods that wrap the active_shipping plugin.'
   s.description = 'Spree extension for providing shipping methods that wrap the active_shipping plugin.'
-  s.required_ruby_version = '>= 2.1.0'
+  s.required_ruby_version = '>= 2.0.0'
   s.rubygems_version      = '>= 1.8.23'
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.require_paths = ["lib"]
 
-  s.add_dependency('spree_core', '~> 3.1.0.beta')
+  s.add_dependency 'spree_core', '~> 3.0.4.beta'
   s.add_dependency('active_shipping', '~> 1.4.2')
   s.add_development_dependency 'pry'
   s.add_development_dependency 'webmock'
